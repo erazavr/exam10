83c5309416b2ef35e325af86dc7e33858052c739
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-02-22 18:54:32 +0600

    Удалил ненужный импорт

--- news-frontend/src/components /Layout/Layout.js
+++ news-frontend/src/components /Layout/Layout.js
@@ -1,7 +1,5 @@
 import React, {Fragment} from 'react';
 import './Layout.css'
-import {NavLink} from "react-router-dom";
-
 const Layout = props => (
     <Fragment>
         <header className='border-bottom mt-3 pb-3 pl-5'>News</header>
